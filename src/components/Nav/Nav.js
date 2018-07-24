85c138f34f56a3e44bfe0f251adbf57238663263
Okens <ng.eric314@gmail.com>
2018-07-24 14:52:51 -0700

    Updating with a simple navbar.

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.css';
+
+const Navbar = () => (
+  <nav className="navbar sticky-top border bg-light h-100vp m-3 align-items-none d-block">
+    <a className="navbar-brand text-dark d-block" href="/">Eric Ng</a>
+    <hr />
+    <a className="nav-item d-block" href="/">Home</a>
+    <a className="nav-item d-block" href="/projects">Projects</a>
+    <a className="nav-item d-block" href="/contact">Contact</a>
+  </nav>
+)
+
+export default Navbar;
