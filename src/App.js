51e31fa56f27a6c6129e552dfccc12c555188a1b
Okens <ng.eric314@gmail.com>
2018-07-31 14:10:46 -0700

    Adding react-router-dom with switch and a router. Creating folder for a route NoMatch page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import Nav from './components/Nav';
 
@@ -7,18 +8,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="">
-        <Nav />
-        <div className="App w-100">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+      <Router>
+        <div className="">
+          <Nav />
+          <Switch>
+            <div className="App w-100">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+              </p>
+            </div>
+          </Switch>
         </div>
-      </div>
+      </Router>
     );
   }
 }
