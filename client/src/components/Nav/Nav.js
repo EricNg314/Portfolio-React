9a238d49ec2bafe92fa8b502ae2d93df5751fa6a
Okens <Ng.Eric314@gmail.com>
2018-09-28 18:10:36 -0700

    Adding updated favicon.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {HashLink as Link} from 'react-router-hash-link';
+import { HashLink as Link } from 'react-router-hash-link';
 import './Nav.css';
 
 const Navbar = () => (
   <nav className="navbar sticky-top border bg-light h-100vp m-2 align-items-none d-block pos-fix box-shadow">
     <Link className="navbar-brand text-dark d-block" to="/">Eric Ng</Link>
     <hr />
-    <Link 
-      to="#aboutId" 
-      scroll={el => el.scrollIntoView({behavior: 'smooth', block:'start'})}
+    <Link
+      to="#aboutId"
+      scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })}
       className="d-block">About</Link>
-    <Link 
-      to="#galleryId" 
-      scroll={el => el.scrollIntoView({behavior: 'smooth', block:'start'})}
-    className="d-block">Gallery</Link>
-    <Link smooth 
+    <Link
+      to="#galleryId"
+      scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })}
+      className="d-block">Gallery</Link>
+    <Link smooth
       to="#contactId"
-      scroll={el => el.scrollIntoView({behavior: 'smooth', block:'start'})}
+      scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })}
       className="d-block">Contact</Link>
   </nav>
 )
