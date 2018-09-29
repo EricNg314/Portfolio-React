10a3fb5c6daefb95c429d519fca56167a007d7bb
Okens <Ng.Eric314@gmail.com>
2018-09-28 23:53:55 -0700

    Correcting issue with image loading through css due to absolute referencing. Adding a cors anywhere. Passing projects as a prop to gallery.

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,17 +1,22 @@
 import React from "react";
 import './Gallery.css';
 
-const Gallery = () => (
+const Gallery = props =>{
+  console.log(props['tagBtnList']);
+  console.log(props['projects']);
+  return (
   <div id="galleryId">
     <div className="parallax-100vh gallery_bg-img1">
         <br></br>
         <div className="container">
           <h2 className="text-center">Gallery</h2>
+
+
         </div>
 
     </div>
   </div>
-);
+)};
 
 export default Gallery;
 
