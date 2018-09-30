dd9efb3d0d6ec0b312f36db0f25607b2fc244f38
Okens <Ng.Eric314@gmail.com>
2018-09-30 14:17:08 -0700

    Updating App.js to store only array in state.projects, Gallery contains a function to projects filter based on an array of tags. Adding tagbuttons without functionality.

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,22 +1,67 @@
-import React from "react";
+import React from 'react';
 import './Gallery.css';
+import TagBtns from '../TagBtns';
 
-const Gallery = props =>{
-  console.log(props['tagBtnList']);
-  console.log(props['projects']);
+const Gallery = props => {
+  const tagBtnList = props['tagBtnList'];
+  const projects = props['projects'];
+  console.log(tagBtnList);
+  console.log(projects);
+
+  const displayProjects = (tagReqArr) => {
+    let display = projects.filter(project => {
+
+      let containsAll = tagReqArr.length;
+      let count = 0;
+
+      for (let i = 0; i < tagReqArr.length; i++) {
+        if (project['tags'].includes(tagReqArr[i])) {
+          count++;
+        }
+      }
+
+      if (count === containsAll) {
+        return true;
+      }
+      return false;
+
+    })
+
+    return display
+  };
+
+
+  console.log(displayProjects(['Show All', 'React']));
+
+
+
   return (
-  <div id="galleryId">
-    <div className="parallax-100vh gallery_bg-img1">
+    <div id="galleryId">
+      <div className="parallax-bg gallery_bg-img1">
         <br></br>
         <div className="container">
           <h2 className="text-center">Gallery</h2>
+          <div className="row">
+            <div className="col-3">
+              {tagBtnList.map((tagName, index) => (
+                <TagBtns 
+                key={index}
+                tagName = {tagName}
+                
+                />
+              ))}
+            </div>
+            <div className="col-9">
 
+            </div>
+          </div>
 
         </div>
 
+      </div>
     </div>
-  </div>
-)};
+  )
+};
 
 export default Gallery;
 
