3f415165cb6c8509750f4883e7b015e2075a461b
Okens <Ng.Eric314@gmail.com>
2018-10-01 00:14:00 -0700

    Updating styles for navbar. Adding button to beginning page to scroll to about. Adding selectTag function passing through Gallery.js. Adding tagname to state update when tag buttons are clicked for list. Updating API.js to remove cors for performance.

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TagBtns from '../TagBtns';
 import ProjectCard from '../ProjectCard';
 
@@ -7,38 +7,7 @@ import './Gallery.css';
 const Gallery = props => {
   const tagBtnList = props['tagBtnList'];
   const projects = props['projects'];
-  // let tagReqArr = props['tagReqArr']
-
-  console.log(tagBtnList);
-  console.log(projects);
-
-  const displayProjects = (tagReqArr) => {
-    let display = projects.filter(project => {
-
-      let containsAll = tagReqArr.length;
-      let count = 0;
-
-      for (let i = 0; i < tagReqArr.length; i++) {
-        if (project['tags'].includes(tagReqArr[i])) {
-          count++;
-        }
-      }
-
-      if (count === containsAll) {
-        return true;
-      }
-      return false;
-
-    })
-
-    return display
-  };
-
-
-  console.log(displayProjects(['Show All']));
-
-
-
+  
   return (
     <div id="galleryId">
       <div className="parallax-bg gallery_bg-img1">
@@ -51,13 +20,13 @@ const Gallery = props => {
                 <TagBtns
                   key={index}
                   tagName={tagName}
-
+                  selectTag={props.selectTag}
                 />
               ))}
             </div>
             <div className="col-9">
               <div className="row">
-                {displayProjects(['Show All']).map((project, index) => (
+                {projects.map((project, index) => (
                   <ProjectCard
                     key={index}
                     project={project}
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,7 +4,9 @@ import './Nav.css';
 
 const Navbar = () => (
   <nav className="navbar sticky-top border bg-light h-100vp m-2 align-items-none d-block pos-fix box-shadow">
-    <Link className="navbar-brand text-dark d-block" to="/">Eric Ng</Link>
+    <Link 
+      to="/"
+      className="navbar-brand text-dark d-block">Eric Ng</Link>
     <hr />
     <Link
       to="#aboutId"
