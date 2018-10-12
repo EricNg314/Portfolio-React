b755ed27a0ddc4a46c134b7a1082a473ef7f10d2
Okens <Ng.Eric314@gmail.com>
2018-10-12 16:40:29 -0700

    Adding future options for customizing tags or clear customization. Passing tag required array to TagBtns component for active check.

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -7,7 +7,7 @@ import './Gallery.css';
 const Gallery = props => {
   const tagBtnList = props['tagBtnList'];
   const projects = props['projects'];
-  
+
   return (
     <div id="galleryId">
       <div className="parallax-bg gallery_bg-img1">
@@ -15,12 +15,19 @@ const Gallery = props => {
         <div className="container">
           <h2 className="text-center">Gallery</h2>
           <div className="row">
-            <div className="col-3">
+            <div className="col-3 p-0 tag-box">
+              <div className="border bg-light rounded text-center mb-1">
+                <h5 className="d-block">Tag Selection</h5>
+                <label className="d-inline-block mx-2"><input type="radio" name="tagOptions" />Custom</label>
+                {/* <label className="d-inline-block mx-2"><input type="radio" name="tagOptions" />Show All</label> */}
+                <label className="d-inline-block mx-2"><input type="radio" name="tagOptions" />Clear</label>
+              </div>
               {tagBtnList.map((tagName, index) => (
                 <TagBtns
                   key={index}
                   tagName={tagName}
                   selectTag={props.selectTag}
+                  tagReqArr = {props.tagReqArr}
                 />
               ))}
             </div>
