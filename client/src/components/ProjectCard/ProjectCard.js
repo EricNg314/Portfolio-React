c2e2b366df56e8192cb3484ec6b63b4d8a0cff24
Okens <Ng.Eric314@gmail.com>
2018-10-12 16:23:27 -0700

    Updating project cards with simple name, hover effect, and link to repositories.

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -6,20 +6,22 @@ const ProjectCard = props => {
   console.log("." + props['project']['image_ref'].slice(6));
   const image_ref = "." + props['project']['image_ref'].slice(6);
   // console.log(image_ref)
-  
+
   return (
-  <div className="col-sm-12 col-md-6 col-lg-6">
-    <div className="portfolio-card card mx-auto mb-4">
-      <img src={image_ref} className="card-img-top" >
-      
-      </img>
+    <div className="col-sm-12 col-md-6 col-lg-6">
+      <div className="portfolio-card card mx-auto mb-4">
+        <a href={props['project']['repo_url']} className='hvr-grow'>
+          <img src={image_ref} className="card-img-top">
+          </img>
+        </a>
+        <span className='text-center'>{props['project']['name']}</span>
+      </div>
     </div>
 
-  </div>
 
 
-
-)}
+  )
+}
 
 export default ProjectCard;
 
