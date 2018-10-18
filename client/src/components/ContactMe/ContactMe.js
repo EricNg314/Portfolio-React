ca4be36be99c42109ce8c4d5dba14525cfc5c43a
Okens <ng.eric314@gmail.com>
2018-10-18 16:37:07 -0700

    Adding folder and temporary files for contact section.

---
+++ client/src/components/ContactMe/ContactMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import ContactMeForm from '../ContactMeForm';
+import './ContactMe.css';
+
+const ContactMe = props => (
+  <div id="contactMeId">
+    <h2>Contact Me</h2>
+
+  </div>
+)
+
+
+export default ContactMe;
