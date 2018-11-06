9cb13b3a9db2df5cc5120efb503a5563bfb5cbea
Okens <Ng.Eric314@gmail.com>
2018-11-06 01:15:05 -0800

    Adding form info to be submitted in contactForm backend function.

--- serverless/contactForm.js
+++ serverless/contactForm.js
@@ -4,12 +4,40 @@ module.exports.sendContactForm = async (event, context) => {
 
   const CONTACT_FORM_URL = process.env.CONTACT_FORM_URL;
 
-  let formData = {}
-  formData[process.env.CONTACT_FORM_NAME_KEY] = 'Name'
-  formData[process.env.CONTACT_FORM_EMAIL_KEY] = 'Email'
-  formData[process.env.CONTACT_FORM_TYPEOFPROFESSION_KEY] = 'Type Of Profession'
-  formData[process.env.CONTACT_FORM_TYPEOFPROFESSION_OTHER_KEY] = 'Type Of Profession Other'
-  formData[process.env.CONTACT_FORM_MESSAGE_KEY] = 'Message'
+  let parsed;
+
+  try {
+    parsed = JSON.parse(event.body);
+  } catch (err) {
+    console.error(`Failed to parse JSON ${event.body}: ${err.stack}`);
+    return {
+      statusCode: 500,
+      error: `Could not parse JSON.`
+    };
+  };
+
+  // Validating variables information
+  const formName = parsed.name;
+  const formEmail = parsed.email;
+  let formTypeOfProfOther = parsed.typeOfProfOther;
+  let formTypeOfProf = parsed.typeOfProf;
+  const formMessage = parsed.message;
+
+  if (formTypeOfProfOther !== ''){
+    formTypeOfProf = '__other_option__'
+  };
+
+  console.log(formName);
+
+  // Creating form object to be posted.
+  let formData = {};
+  formData[process.env.CONTACT_FORM_NAME_KEY] = formName;
+  formData[process.env.CONTACT_FORM_EMAIL_KEY] = formEmail;
+  formData[process.env.CONTACT_FORM_TYPEOFPROFESSION_OTHER_KEY] = formTypeOfProfOther;
+  formData[process.env.CONTACT_FORM_TYPEOFPROFESSION_KEY] = formTypeOfProf;
+  formData[process.env.CONTACT_FORM_MESSAGE_KEY] = formMessage;
+
+  // TODO: Send HTTP Request.
 
 
   return {
